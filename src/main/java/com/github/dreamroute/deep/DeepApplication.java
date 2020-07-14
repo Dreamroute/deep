db369510c7b6e8be3c16cd56d6ba94dc90f74bcd
w.dehai <wangdehai@bdfint.com>
2020-07-14 18:21:03 +0800

    init.

---
+++ src/main/java/com/github/dreamroute/deep/DeepApplication.java
@@ -0,0 +1,13 @@
+package com.github.dreamroute.deep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/github/dreamroute/deep/domain/User.java
@@ -0,0 +1,15 @@
+package com.github.dreamroute.deep.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Long id;
+    private String name;
+}
