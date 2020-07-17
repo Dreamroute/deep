259f099d68ba86ad3d0f7c497edfe1bd9d7109da
w.dehai <342252328@qq.com>
2020-07-17 12:33:51 +0800

    init.

---
+++ src/test/java/com/github/dreamroute/deep/misc/SpringJM.java
@@ -0,0 +1,20 @@
+package com.github.dreamroute.deep.misc;
+
+import com.github.dreamroute.deep.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.ApplicationContext;
+
+public class SpringJM {
+
+    @Test
+    public void mm0() {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        BeanDefinitionRegistry registry;
+        new RootBeanDefinition(User.class);
+        ApplicationContext container;
+    }
+
+}
