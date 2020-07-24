4677c69affcea5b257b7084d82c00f1e27aa4b82
w.dehai <342252328@qq.com>
2020-07-24 17:57:03 +0800

    init.

--- src/main/java/com/github/dreamroute/deep/mapper/UserMapper.java
+++ src/main/java/com/github/dreamroute/deep/mapper/UserMapper.java
@@ -1,10 +1,13 @@
 package com.github.dreamroute.deep.mapper;
 
 import com.github.dreamroute.deep.domain.User;
-import org.apache.ibatis.annotations.Param;
 
 public interface UserMapper {
 
-    int insert(@Param("user") User user);
+    int insert(User user);
+
+    User findByNameAndPassword(String name, String password);
+
+    User selectById(Long id);
 
 }
