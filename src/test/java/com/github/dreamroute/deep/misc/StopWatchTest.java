a8ab24611b6aa5ce3f9882baea731609291033e2
w.dehai <342242328@qq.com>
2020-08-02 01:08:47 +0800

    111

--- src/test/java/com/github/dreamroute/deep/misc/StopWatchTest.java
+++ src/test/java/com/github/dreamroute/deep/misc/StopWatchTest.java
@@ -2,6 +2,11 @@ package com.github.dreamroute.deep.misc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.util.StopWatch;
+import sun.rmi.transport.StreamRemoteCall;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class StopWatchTest {
 
@@ -13,5 +18,23 @@ public class StopWatchTest {
         sw.stop();
         System.err.println(sw.getTotalTimeSeconds());
     }
+
+    @Test
+    public void optionalTest() {
+        String name = "w.dehai";
+        Optional<String> op = Optional.ofNullable(name);
+        Optional<String> s = op.filter(v -> Objects.equals("w.dehai", v));
+        s.ifPresent(System.err::println);
+
+        op.map(v -> v + "111").ifPresent(System.err::println);
+    }
+
+    @Test
+    public void streamTest() {
+        Stream<Integer> stream = Stream.of(1, 2, 3);
+//        User : id: name
+        stream.map()
+    }
+
 
 }
